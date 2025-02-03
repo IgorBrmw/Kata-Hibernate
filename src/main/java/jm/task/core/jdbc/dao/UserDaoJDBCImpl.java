e2077ee47b3744cd244ba9813621a161191a94ec
Igor Burmistrov <igor.brmw@gmail.com>
2025-02-03 09:46:27 +0300

    My jdbc work

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,92 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import javax.xml.transform.Result;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
 
+    private ArrayList<User> users;
+    private final Util util;
+    Connection connection = Util.getConnection();
+
+    String addUser = "insert into users (name, lastName, age) values (?, ?, ?)";
+    String deleteUser = "delete from users where id=?";
+    String dropUsersTable = "DROP TABLE IF EXISTS users";
+    String createUsersTable = "create table if not exists users (id int auto_increment primary key," +
+            " name varchar(50) not null, lastName varchar(50) not null, age int not null);";
+    String query = "select id, name, lastName, age FROM users";
+    String deleteFrom = "delete from users";
+
+    public UserDaoJDBCImpl(Util util) throws SQLException {
+        this.util = util;
+        this.users = new ArrayList<>();
     }
 
     public void createUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(createUsersTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(dropUsersTable);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        try (PreparedStatement statement = connection.prepareStatement(addUser)) {
+            statement.setString(1, name);
+            statement.setString(2, lastName);
+            statement.setByte(3, age);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        try (PreparedStatement statement = connection.prepareStatement(deleteUser)) {
+            statement.setLong(1, id);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public List<User> getAllUsers() {
-        return null;
+
+        try (Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery(query)) {
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong(1));
+                user.setName(resultSet.getString(2));
+                user.setLastName(resultSet.getString(3));
+                user.setAge((byte) resultSet.getInt(4));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(deleteFrom);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
+
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USER = "root";
+    private static final String PASS = "rootqwe";
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = DriverManager.getConnection(URL, USER, PASS);
+        return connection;
+    }
 }
